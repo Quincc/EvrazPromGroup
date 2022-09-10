39cc4440c343c9f6f50014330934766832227ed9
Максим Головченко <maskim.tradeof@gmail.com>
2022-09-10 21:16:45 +0300

    test

--- EvrazPromGroup/Form1.cs
+++ EvrazPromGroup/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,17 @@ namespace EvrazPromGroup
         }
         private bool Cheack_User(string login, string password)
         {
+            using (StreamReader reader = new StreamReader("Users"))
+            {
+                string text = reader.ReadToEnd();
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text. == $"{login} {password}")
+                    {
+
+                    }
+                }    
+            }
             return false;
         }
     }
